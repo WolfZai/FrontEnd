4217bb9253238c17dfe7b259dcdc3b154cf8e607
Tommy <tommylebreton8@gmail.com>
2025-02-27 09:22:17 +0100

    refactorisation + class active

--- assets/script.js
+++ assets/script.js
@@ -1,125 +1,122 @@
-document.addEventListener("DOMContentLoaded", function () {
-  async function Init() {
-    await fetchWorks();
-    fetchCategories();
-  }
+function Init() {
+  fetchWorks();
+  fetchCategories();
+}
 
-  let logout = document.getElementById("connexion");
-  let categoriesContainer = document.querySelector(".category");
+let logout = document.getElementById("connexion");
+let categoriesContainer = document.querySelector(".category");
 
-  if (localStorage.getItem("token")) {
-    logout.innerText = "logout";
-    console.log(localStorage.getItem("token"));
-    categoriesContainer.style.display = "none";
+if (localStorage.getItem("token")) {
+  logout.innerText = "logout";
+  console.log(localStorage.getItem("token"));
+  categoriesContainer.style.display = "none";
+} else {
+  logout.innerText = "login";
+  categoriesContainer.style.display = "flex";
+}
+
+logout.addEventListener("click", function () {
+  if (logout.innerText === "login") {
+    window.location.href = "./login.html";
   } else {
+    localStorage.removeItem("token");
     logout.innerText = "login";
     categoriesContainer.style.display = "flex";
   }
+});
 
-  logout.addEventListener("click", function () {
-    if (logout.innerText === "login") {
-      window.location.href = "./login.html";
-    } else {
-      localStorage.removeItem("token");
-      logout.innerText = "login";
-      categoriesContainer.style.display = "flex";
+let allWorks = [];
+
+async function fetchWorks() {
+  try {
+    const response = await fetch("http://localhost:5678/api/works");
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
     }
-  });
-
-  let allWorks = [];
-
-  async function fetchWorks() {
-    try {
-      const response = await fetch("http://localhost:5678/api/works");
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
-      const works = await response.json();
-      console.log(works);
-      allWorks = works;
-      displayFilterWorks(works);
-    } catch (error) {
-      console.error("error récup", error);
-    }
+    const works = await response.json();
+    console.log(works);
+    allWorks = works;
+    displayFilterWorks(works);
+  } catch (error) {
+    console.error("error récup", error);
   }
+}
 
-  async function fetchCategories() {
-    try {
-      const response = await fetch("http://localhost:5678/api/categories");
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
-      const categories = await response.json();
-      console.log(categories);
-      createFilters(categories);
-    } catch (error) {
-      console.error("error récup", error);
+async function fetchCategories() {
+  try {
+    const response = await fetch("http://localhost:5678/api/categories");
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
     }
+    const categories = await response.json();
+    console.log(categories);
+    createFilters(categories);
+  } catch (error) {
+    console.error("error récup", error);
   }
+}
 
-  function createFilters(categories) {
-    const container = document.querySelector(".category");
-    container.innerHTML = "";
+function createFilters(categories) {
+  const container = document.querySelector(".category");
+  container.innerHTML = "";
 
-    const allButton = document.createElement("a");
-    allButton.textContent = "Tous";
+  const allButton = document.createElement("a");
+  allButton.textContent = "Tous";
+  allButton.classList.add("active");
+  allButton.addEventListener("click", function () {
+    document
+      .querySelectorAll(".category a")
+      .forEach((lien) => lien.classList.remove("active"));
     allButton.classList.add("active");
-    allButton.addEventListener("click", function () {
-      document
-        .querySelectorAll(".category a")
-        .forEach((lien) => lien.classList.remove("active"));
-      allButton.classList.add("active");
-      displayFilterWorks(allWorks);
-    });
-    container.appendChild(allButton);
+    displayFilterWorks(allWorks);
+  });
+  container.appendChild(allButton);
 
-    categories.forEach((category) => {
-      const lienCategory = document.createElement("a");
-      lienCategory.textContent = category.name;
-      lienCategory.addEventListener("click", function () {
-        document.querySelectorAll(".category a").forEach((lien) => {
-          lien.classList.remove("active");
-        });
-        lienCategory.classList.add("active");
-        filterCategory(category.id);
+  categories.forEach((category) => {
+    const lienCategory = document.createElement("a");
+    lienCategory.textContent = category.name;
+    lienCategory.addEventListener("click", function () {
+      document.querySelectorAll(".category a").forEach((lien) => {
+        lien.classList.remove("active");
       });
-      container.appendChild(lienCategory);
+      lienCategory.classList.add("active");
+      filterCategory(category.id);
     });
+    container.appendChild(lienCategory);
+  });
+}
+
+function filterCategory(categoryId) {
+  const filteredWorks = allWorks.filter((work) => {
+    console.log(`Comparaison ${work.categoryId} === ${categoryId}`);
+    return work.categoryId == categoryId;
+  });
+  console.log("Résultat filtrée :", filteredWorks);
+
+  displayFilterWorks(filteredWorks);
+}
+
+function displayFilterWorks(filteredWorks) {
+  const container = document.querySelector(".gallery");
+  container.innerHTML = "";
+
+  if (filteredWorks.length === 0) {
+    console.log("les works ne sont pas encore chargés");
+
+    return;
   }
 
-  function filterCategory(categoryId) {
-    const filteredWorks = allWorks.filter((work) => {
-      console.log(`Comparaison ${work.categoryId} === ${categoryId}`);
-      return work.categoryId == categoryId;
-    });
-    console.log("Résultat filtrée :", filteredWorks);
-
-    displayFilterWorks(filteredWorks);
-  }
-
-  function displayFilterWorks(filteredWorks) {
-    const container = document.querySelector(".gallery");
-    container.innerHTML = "";
-
-    if (filteredWorks.length === 0) {
-      console.log("les works ne sont pas encore chargés");
-
-      return;
-    }
-
-    filteredWorks.forEach((work) => {
-      const figure = document.createElement("figure");
-      figure.className = "work";
-      figure.innerHTML = `
+  filteredWorks.forEach((work) => {
+    const figure = document.createElement("figure");
+    figure.className = "work";
+    figure.innerHTML = `
     <img src="${work.imageUrl}" alt="${work.title}" />
     <figcaption>${work.title}</figcaption>
     `;
-      container.appendChild(figure);
-    });
-  }
-  Init();
-});
-
+    container.appendChild(figure);
+  });
+}
+Init();
 // fetch("http://localhost:5678/api/works")
 //   .then((response) => response.json())
 //   .then((data) => {
